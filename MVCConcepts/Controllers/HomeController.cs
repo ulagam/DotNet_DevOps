33c7ad3f6436d9f71c7ae67f2d4854f5261cc40b
ulagam <32107409+ulagam@users.noreply.github.com>
2017-09-20 15:40:03 +0530

    Add files via upload

---
+++ MVCConcepts/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCConcepts.UtilityFilter;
+
+namespace MVCConcepts.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/      
+        [CustomActionFilter]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCConcepts/UtilityFilter/CustomActionFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCConcepts.UtilityFilter
+{
+    public class CustomActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.OnActionExecuting = "OnActionExecuting 1";
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            filterContext.Controller.ViewBag.OnActionExecuted = "OnActionExecuted 2";
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.OnResultExecuting = "OnResultExecuting 3";
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            filterContext.Controller.ViewBag.OnResultExecuted = "OnActionExecuting 4";
+        }
+    }
+}
